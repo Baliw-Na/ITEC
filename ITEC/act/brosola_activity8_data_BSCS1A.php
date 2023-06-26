e04c3166dcd4d6940a9b0618e3e5f4161a488461
Baliw-Na <114274285+Baliw-Na@users.noreply.github.com>
2023-06-26 22:35:56 +0800

    Add files via upload

---
+++ ITEC/act/brosola_activity8_data_BSCS1A.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost"; // Replace with your MySQL server name
+$username = "root"; // Replace with your MySQL username
+$password = ""; // Replace with your MySQL password
+$database = "test"; // Replace with your MySQL name
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+//Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+//Retrieve form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+//Prepare the INSERT statement
+$sql = "INSERT INTO `table` (name, email, message) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt ->bind_param("sss", $name, $email, $message);
+
+//Execute the statement
+if ($stmt->execute()) {
+	echo "Form data saved successfully.";
+} else{	
+	echo "Error: " . $stmt->error;
+}
+//Close the connection
+$stmt->close();
+$conn->close();
+
+?>
